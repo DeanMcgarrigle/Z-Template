8c19e6d3a13ddb3b21e488da859a730bcf74edff
DeanMcgarrigle <dean.mcgarrigle@gmail.com>
2016-07-06 14:36:45 +0100

    funky button!

--- app/js/controllers/home.js
+++ app/js/controllers/home.js
@@ -1,13 +1,29 @@
-module.exports = function (common) {
+module.exports = function (common, $scope, $timeout) {
 
     var vm = this;
-   
+
     activate();
 
     function activate() {
         common.activateController([], 'home').then(() => {
             // log('success', 'Home', 'Page loaded..');
+            $timeout(function(){
+                vm.topDirections = ['left', 'up'];
+                vm.bottomDirections = ['down', 'right'];
+
+                vm.isOpen = false;
+
+                vm.availableModes = ['md-fling', 'md-scale'];
+                vm.selectedMode = 'md-fling';
+
+                vm.availableDirections = ['up', 'down', 'left', 'right'];
+                vm.selectedDirection = 'up';
+            }, 0);
+            
         });
     }
+
+    // $scope.isOpen = false;
+    // console.log($scope.isOpen)
 }
 ;
