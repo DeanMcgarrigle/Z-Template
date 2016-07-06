1d99df43a4270c1e25a000d6022247dca4631904
DeanMcgarrigle <dean.mcgarrigle@gmail.com>
2016-07-06 15:33:28 +0100

    added material icon pack

--- webpack.config.js
+++ webpack.config.js
@@ -14,10 +14,17 @@ module.exports = {
         loaders: [
             { test: /\.html/, loader: 'raw' },
             { test: /\.js$/, loader: 'babel', query: {cacheDirectory: true, presets: ['es2015']},exclude: /node_modules/ },
-            {test: /\.css$/, loader: 'style-loader!css-loader'},
-            {test: /\.scss$/, loader: "style-loader!css-loader!sass-loader?"},
-            {test: /\.less$/, loader: 'style-loader!css-loader!less-loader'},
-            { test: /\.json/, loader: 'json' }
+            { test: /\.css$/, loader: 'style-loader!css-loader'},
+            { test: /\.scss$/, loader: "style-loader!css-loader!sass-loader?"},
+            { test: /\.less$/, loader: 'style-loader!css-loader!less-loader'},
+            { test: /\.json/, loader: 'json' },
+            { test: /\.ico$/, loader: 'url-loader?mimetype=image/ico'},
+            { test: /\.(jpg|png|gif)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file-loader?name=img/[name].[ext]'},
+            {
+                test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: "url-loader?name=fonts/[name].[ext]&limit=10000&minetype=application/font-woff"
+            },
+            { test: /\.(ttf|eot|svg)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader?name=fonts/[name].[ext]"},
         ]
     },
     resolve: {
